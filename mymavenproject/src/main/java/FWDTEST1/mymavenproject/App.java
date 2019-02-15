8d8d9892076635b14e2f3f736c9f324b152ac1ea
TMPrasanth <TMPrasanth@DESKTOP-IVS135V>
2019-02-15 17:52:05 +0530

    committed

---
+++ mymavenproject/src/main/java/FWDTEST1/mymavenproject/App.java
@@ -0,0 +1,13 @@
+package FWDTEST1.mymavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
